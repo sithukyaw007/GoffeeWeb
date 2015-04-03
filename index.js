87672f155dae96500412f9e39cf8e0b9fc27daba
ChesterCheong <wtcheong.2012@sis.smu.edu.sg>
2015-04-03 07:31:03 +0000

    test

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ var app = express();
 var model = require('./model.js');
 var paypal = require('paypal-rest-sdk');
 //
+//tells the server to look into the /public folder for the static content
+app.use(express.static(__dirname + '/public'));
+
 paypal.configure({
     'mode': process.env.PAYPAL_MODE, //sandbox or live
     'client_id': process.env.PAYPAL_CLIENT_ID,
     'client_secret': process.env.PAYPAL_CLIENT_SECRET
 });
 
-//tells the server to look into the /public folder for the static content
-app.use(express.static(__dirname + '/public'));
-
 //Creates the plans based on what is defined in model.js. 
 app.get('/payment/create-plan', function (req, res) {
     var plans = Object.keys(model.plans);
+
     for(var i = 0; i < plans.length; i++){
         //goes through each of the plans defined in model.js
         var plan = model.plans[plans[i]];
+            console.log(plan);
         paypal.billingPlan.create(plan, function(error, billingPlan){
             //creates the plan
+            
             if(error){
                 throw error;
             }
