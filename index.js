1c7a334996083a1a309073485029d1dc33ef97d2
ChesterCheong <wtcheong.2012@sis.smu.edu.sg>
2015-04-04 11:25:35 +0000

    debug

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ var express = require('express');
 var app = express();
 var model = require('./model.js');
 var paypal = require('paypal-rest-sdk');
-var bodyParser = require('body-parser');
-var uid;
-//
-//tells the server to look into the /public folder for the static content
-app.use(express.static(__dirname + '/public'));
-
-app.use(bodyParser.json());
 
 paypal.configure({
     'mode': process.env.PAYPAL_MODE, //sandbox or live
@@ -16,34 +9,38 @@ paypal.configure({
     'client_secret': process.env.PAYPAL_CLIENT_SECRET
 });
 
+//tells the server to look into the /public folder for the static content
+app.use(express.static(__dirname + '/public'));
+
 //Creates the plans based on what is defined in model.js. 
 app.get('/payment/create-plan', function (req, res) {
     var plans = Object.keys(model.plans);
-
     for(var i = 0; i < plans.length; i++){
         //goes through each of the plans defined in model.js
         var plan = model.plans[plans[i]];
         paypal.billingPlan.create(plan, function(error, billingPlan){
             //creates the plan
-
             if(error){
+                res.json({'status':'error1'});
                 throw error;
             }
             //activates the plan
             paypal.billingPlan.update(billingPlan.id, model.activatePlan, function(error, response){
                 if(error){
+                    res.json({'status':'error2'});
                     throw error;
                 }
                 var plan = "";
                 //sets the plan id based on the plan name sent to PayPal
                 if(billingPlan.name == "Regular Plan"){
-                    plan = "2999";
+                    plan = "2000";
                 }
                 else{
-                    plan = "5999";
+                    plan = "10000";
                 }
                 //stores the PayPal Plan ID to your plan id mapping in Firebase
                 model.firebase.child('/plans').child('/' + plan).set({'id': billingPlan.id});
+                res.json({'status':'plansuccess'});
             });
         });
     }
@@ -59,12 +56,12 @@ app.get('/payment/initiate/:planId', function (req, res) {
         plans = plans.val();
         //checks if the plan exists in Firebase
         if(plans[planId]){
+            //if the plan exists, create a BillingAgreement payload using the planid that is passed in
             var billingAgreement = model.createAgreementData(planId, plans[planId].id, model.address);
             paypal.billingAgreement.create(billingAgreement, function(error, agreement){
                 //creates the billing agreement
-                console.json(error);
                 if(error){
-                    //throw error;
+                    throw error;
                 }
                 //if creating the billing agreement is successful, find the approval url and redirect the user to it
                 for(var i = 0; i < agreement.links.length; i++){
@@ -75,7 +72,7 @@ app.get('/payment/initiate/:planId', function (req, res) {
                 }
                 //if approval_url is not found, throw an error
                 res.json({'status': 'failed'});
-                //throw "approval_url not found";
+                throw "approval_url not found";
             });
         }
         else{
@@ -93,7 +90,8 @@ app.get('/payment/execute/', function (req, res) {
         //starts the billingAgreement and collects the money
         paypal.billingAgreement.execute(req.query.token, {}, function(error, agreement){
             if(error){
-                throw error;
+                console.log(error);
+                //throw error;
             }
             else{
                 res.json({'status':'success', 'data': agreement});
@@ -104,11 +102,7 @@ app.get('/payment/execute/', function (req, res) {
         res.json({'status':'failed'})
     }
 })
-app.post('/login', function(req, res) {
-    uid = req.body.uid;
-    res.set('Content-Type', 'application/json'); // tell Angular that this is JSON
-    res.send(JSON.stringify({success: 'success'}));
-})
+
 //cancels a specific agreement
 app.get('/payment/cancel/:agreementId', function(req, res){
     var cancel_note = {'note':'cancel'};
